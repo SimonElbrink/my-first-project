63789d1943b1875f1018a4976fb10342665dabac
Simon Elbrink <simon.elbrink@lexicon.se>
2022-05-31 10:43:09 +0200

    I want to demo Git commits and pushing it to Github! with at simple calculation.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, and Welcome to this java course! - Java Group 43!");
+        System.out.println(10 + 15);
     }
 }
