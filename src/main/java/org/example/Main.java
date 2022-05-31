9d367ce2bbe706df5be49a14ee07a6ee5d18105b
Simon Elbrink <simon.elbrink@lexicon.se>
2022-05-31 10:36:45 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, and Welcome to this java course! - Java Group 43!");
+    }
+}
